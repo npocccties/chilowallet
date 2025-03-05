966a99277cc11b371c420746cc9b3047354b8fd1
etou.yoshinobu <yoshinobu.eto@j-tech.jp>
2025-03-05 14:51:12 +0900

    chore: session_cookieがない状態でリクエストするとリダイレクト内容が返却される不具合修正

--- middleware.api.ts
+++ middleware.api.ts
@@ -24,6 +24,10 @@ export async function middleware(req: NextRequest) {
   const session_cookie = req.cookies.get("session_cookie");
 
   if (!session_cookie) {
+    if (url.pathname == api.v1.badge.status_list) {
+      loggerMWInfo(`no session_cookie! returning 401 Unauthorized`);
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
     loggerMWInfo(`no session_cookie! redirect to ${redirectUrl}`);
     return NextResponse.redirect(new URL(redirectUrl, req.url));
   }
